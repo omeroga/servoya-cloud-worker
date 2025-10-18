33bc57937f087389679350c6b89be6b5b6bb9ed5
omeroga <omer@servoya.com>
2025-10-17 21:08:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import express from "express";
+import express from 'express';
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("✅ Servoya Cloud Worker is live!");
+app.get('/', (req, res) => {
+  res.send('Servoya Cloud Worker is live!');
 });
+
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
