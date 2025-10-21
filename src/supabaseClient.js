e769a30410ed0bb790a8040e5e6e57b260948baf
omeroga <omer@servoya.com>
2025-10-21 00:18:16 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL || "https://gpeijpqhpswggkbhnwqq.supabase.co";
-const SUPABASE_KEY = process.env.SUPABASE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZWlqcHFocHN3Z2drYmhud3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5ODQ2MjIsImV4cCI6MjA3NjU2MDYyMn0.AHR1jQAQ1jA8NKc6LSoGybJJQ0w2c3jpVRJdC_BKC04";
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export default supabase;
